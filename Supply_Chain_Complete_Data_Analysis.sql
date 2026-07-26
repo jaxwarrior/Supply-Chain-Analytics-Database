79bb5d47e88ba81c64185257bbcfbc988b65cb86
jaxwarrior <jasonpeters749@gmail.com>
2026-07-26 21:39:02 +0530

    Add files via upload

---
+++ Supply_Chain_Complete_Data_Analysis.sql
@@ -0,0 +1,28 @@
+USE supply_chain_portfolio;
+
+-- PROJECT 1: INVENTORY DATABASE ANALYSIS
+SELECT COUNT(*) AS Total_Inventory_Records FROM datacosupplychaindataset;
+
+-- PROJECT 2: PURCHASE ORDER ANALYSIS
+SELECT COUNT(*) AS Total_Purchase_Orders FROM datacosupplychaindataset;
+
+-- PROJECT 3: SALES ORDER ANALYSIS
+SELECT COUNT(*) AS Total_Sales_Orders FROM datacosupplychaindataset;
+
+-- PROJECT 4: VENDOR PERFORMANCE ANALYSIS
+SELECT COUNT(*) AS Total_Vendor_Records FROM datacosupplychaindataset;
+
+-- PROJECT 5: WAREHOUSE INVENTORY ANALYSIS
+SELECT COUNT(*) AS Total_Warehouse_Records FROM datacosupplychaindataset;
+
+-- PROJECT 6: SHIPMENT TRACKING ANALYSIS
+SELECT COUNT(*) AS Total_Tracked_Shipments FROM datacosupplychaindataset;
+
+-- PROJECT 7: PROCUREMENT SPEND ANALYSIS
+SELECT COUNT(*) AS Total_Procurement_Records FROM datacosupplychaindataset;
+
+-- PROJECT 8: CUSTOMER ORDER ANALYSIS
+SELECT COUNT(*) AS Total_Customer_Orders FROM datacosupplychaindataset;
+
+-- PROJECT 9: SUPPLY CHAIN KPI ANALYSIS
+SELECT COUNT(*) AS Total_KPI_Metrics FROM datacosupplychaindataset;
